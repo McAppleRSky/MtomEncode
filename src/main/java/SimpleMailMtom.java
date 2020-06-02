958b759133f560c0126a05e2ba8dca17400a8747
McAppleRSky <mcapple@yandex.ru>
2020-06-02 18:06:54 +0300

    add base64 class

--- src/main/java/SimpleMailMtom.java
+++ src/main/java/SimpleMailMtom.java
@@ -1,46 +1,47 @@
-import com.sun.tools.javac.util.StringUtils;
-
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
+
 import javax.mail.BodyPart;
 import javax.mail.MessagingException;
 import javax.mail.Part;
 import javax.mail.internet.MimeMultipart;
 import javax.mail.util.ByteArrayDataSource;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 public class SimpleMailMtom {
 
 
-    public byte[] mimeParser(InputStream isMtm) {
-        String ct = "";
-        ByteArrayOutputStream baos = null;
+//    public byte[] mimeParser(InputStream inputStreamMtm, String mtmType) {
+    public byte[] mimeParser(String stringMtm, String mtmType) {
+//        String mtmType = "";
+        ByteArrayOutputStream byteArrayOutputStream = null;
+        MimeMultipart mimeMultipart ;
+        ByteArrayDataSource byteArrayDataSource;
         try {
-            MimeMultipart mp = new MimeMultipart(String.valueOf(new ByteArrayDataSource(isMtm,
-                    ct)));
-            int count = mp.getCount();
-            baos = new ByteArrayOutputStream();
+            byteArrayDataSource = new ByteArrayDataSource(stringMtm, mtmType);
+            mimeMultipart = new MimeMultipart(byteArrayDataSource);
+            int count = mimeMultipart.getCount();
+            byteArrayOutputStream = new ByteArrayOutputStream();
             for (int i = 0; i < count; i++) {
-                BodyPart bodyPart = mp.getBodyPart(i);
+                BodyPart bodyPart = mimeMultipart.getBodyPart(i);
                 if (!Part.ATTACHMENT
                         .equalsIgnoreCase(bodyPart.getDisposition())
                         && !StringUtils.isNotBlank(bodyPart.getFileName())
                 ) {
                     continue; // dealing with attachments only
                 }
-                bodyPart.writeTo(baos);
+                bodyPart.writeTo(byteArrayOutputStream);
             }
 
-            byte[] attachment = baos.toByteArray();
-//            FileUtils.writeByteArrayToFile(new File("E:/wss/attachment.zip"), attachment);
+            byte[] attachment = byteArrayOutputStream.toByteArray();
+            FileUtils.writeByteArrayToFile(new File("./attachment.zip"), attachment);
             return attachment;
         } catch (Exception ex) {
             ex.printStackTrace();
         } finally {
-            if (baos != null) {
+            if (byteArrayOutputStream != null) {
                 try {
-                    baos.close();
+                    byteArrayOutputStream.close();
                 } catch (Exception ex) {
 
                 }
@@ -51,11 +52,21 @@ public class SimpleMailMtom {
 
     public void Encode2File() throws IOException, MessagingException {
         String data = "", contentType = "";
-        MimeMultipart mp = new MimeMultipart(String.valueOf(new ByteArrayDataSource(data, contentType)));
-        int count = mp.getCount();
+        MimeMultipart mimeMultipart = new MimeMultipart(String.valueOf(new ByteArrayDataSource(data, contentType)));
+        int count = mimeMultipart.getCount();
         for (int i = 0; i < count; i++) {
-            BodyPart bp = mp.getBodyPart(i);
-//            bp.saveFile(filepath + "_" + i);
+            BodyPart bodyPart = mimeMultipart.getBodyPart(i);
+//            bodyPart.saveFile(filepath + "_" + i);
+
+            InputStream inputStream = bodyPart.getInputStream();
+            File file = new File("/tmp/" + bodyPart.getFileName());
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            byte[] buf = new byte[4096];
+            int bytesRead;
+            while((bytesRead = inputStream.read(buf))!=-1) {
+                fileOutputStream.write(buf, 0, bytesRead);
+            }
+            fileOutputStream.close();
         }
     }
 }
